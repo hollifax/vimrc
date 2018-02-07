73d4522112d21f21af6feeffb47bf24b05a18c7d
hollifax <hollifax@protonmail.com>
2018-02-07 22:11:28 +0000

    Provide a simple install bash script. Clones the vim plugins VundleVim and pathogen and runs VundleVim install

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp .vimrc ${HOME}
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
